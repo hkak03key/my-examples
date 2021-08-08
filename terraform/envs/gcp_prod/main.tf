ce91737e65d97d1970a550dd7012099f5bb7aa3e
Hikaru <hkak03key@gmail.com>
2021-08-08 18:13:19 +0000

    depends_on

--- terraform/envs/gcp_prod/main.tf
+++ terraform/envs/gcp_prod/main.tf
@@ -2,4 +2,8 @@ module "google_cloudfunctions_printenv" {
   source = "../../modules/google_cloudfunctions_printenv"
 
   tf_bucket = local.tf_bucket
+
+  depends_on = [
+    google_project_service.services,
+  ]
 }
